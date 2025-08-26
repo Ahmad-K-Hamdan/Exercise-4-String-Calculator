94698b84fc5c452b453b348190e6bc6e03b688a8
Ahmad-K-Hamdan <Ahmadkhamdan@outlook.com>
2025-08-26 04:45:42 +0300

    Deleted main program

--- String Calculator/Program.cs
+++ String Calculator/Program.cs
@@ -4,7 +4,5 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var calculator = new Calculator();
-        Console.WriteLine(calculator.Add("10000,15,1000,1001"));
     }
 }
